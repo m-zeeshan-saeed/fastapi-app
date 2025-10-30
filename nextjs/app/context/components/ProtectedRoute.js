622d890d67f92b1cca570cac3da9fe47a9f6cec6
m-zeeshan-saeed <zeeshansheikh0313@gmail.com>
2025-10-30 21:34:27 +0500

    add frontend login page

---
+++ nextjs/app/context/components/ProtectedRoute.js
@@ -0,0 +1,18 @@
+"use client";
+
+import { useContext, useEffect } from "react";
+import { useRouter } from "next-navigation";
+import AuthContext from "../AuthContext";
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useContext(AuthContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/login");
+    }
+  }, [user, router]);
+
+  return user ? children : null;
+};
